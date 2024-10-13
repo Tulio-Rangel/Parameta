b899e8c19c9cf6c0ecee31b10262c3cbd1b4877f
Tulio-Rangel <tjrn20@hotmail.com>
2024-10-12 19:02:27 -0500

    Se agrega unit test para entidad Empleado

--- src/main/java/com/tulio/parameta/repository/EmpleadoRepository.java
+++ src/main/java/com/tulio/parameta/repository/EmpleadoRepository.java
@@ -2,6 +2,8 @@ package com.tulio.parameta.repository;
 
 import com.tulio.parameta.entity.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmpleadoRepository extends JpaRepository<Empleado, Long> {
 }
